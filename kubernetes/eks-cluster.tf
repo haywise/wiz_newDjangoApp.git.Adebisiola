ff3471492847ab208fd692ec32f1307b3f89dd18
Bisi-23 <130673182+Bisi-23@users.noreply.github.com>
2024-02-16 03:02:50 -0600

    Update eks-cluster.tf

--- kubernetes/eks-cluster.tf
+++ kubernetes/eks-cluster.tf
@@ -1,29 +1,13 @@
-provider "kubernetes" {
-    #load_config_file = "false"
-    host = data.aws_eks_cluster.myapp-cluster.endpoint
-    token = data.aws_eks_cluster_auth.myapp-cluster.token
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.myapp-cluster.certificate_authority.0.data)
-}
-
-data "aws_eks_cluster" "myapp-cluster" {
-    name = module.eks.cluster_id
-}
-
-
-data "aws_eks_cluster_auth" "myapp-cluster" {
-    name = module.eks.cluster_id
-}
-
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "18.21.0"
+  version = "~> 19.0"
 
-  cluster_name = "myapp-eks-cluster"  
-  cluster_version = "1.20"
+  cluster_name                  = "myApp-eks-cluster"  
+  cluster_version               = "1.24"
+  cluster_endpoint_public_access = true
 
-  subnet_ids = module.myapp-vpc.private_subnets
-  vpc_id = module.myapp-vpc.vpc_id
+  subnet_ids = module.myApp-vpc.private_subnets
+  vpc_id     = module.myApp-vpc.vpc_id
 
   tags = {
     environment = "development"
@@ -32,12 +16,11 @@ module "eks" {
 
   eks_managed_node_groups = {
     dev = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 2
-
-      instance_types = ["t2.small"]
-      key_name       = "public-kp"
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_types   = ["t2.small"]
+      key_name         = "public-kp"
     }
   }
 }
